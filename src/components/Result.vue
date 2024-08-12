4a9e428ef944b26cfc6280d95b1d6b7cc86f095f
mac-anthony <ekenemacanthony>
2024-08-12 15:33:23 +0100

    commiting the files for the reaction game

---
+++ src/components/Result.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <h4>Your Reaction Time is: {{ score }} ms </h4>
+  <p class="rank"> {{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'], //to accept the prop "score" that was passed in from App.vue component
+    data(){
+        return {
+            rank: null
+        }
+    },
+    mounted(){
+        if (this.score < 250 ){
+            this.rank = "Excellent";
+        } else if ( this.score < 450 ){
+            this.rank = "Nice! Rapid Reflexes"
+        }else {
+            this.rank = "Slow as snail..."
+        }
+    }
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
